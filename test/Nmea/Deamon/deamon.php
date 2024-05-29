f9d60e985606f5b7afd2f27a2bb49dc9881b818a
Frank.Markwort <frank.markwort@syzygy.de>
2024-05-29 09:40:47 +0300

    Add Libs

--- test/Nmea/Deamon/deamon.php
+++ test/Nmea/Deamon/deamon.php
@@ -3,5 +3,5 @@
 use Nmea\Cache\Memcached;
 use Nmea\Deamon\Bootstrap;
 
-$bootstrap = new Bootstrap(new Serial(__DIR__ . '/../../bin/ttyOut'), new Memcached());
+$bootstrap = new Bootstrap(new Serial(__DIR__ . '/../../bin/ttyOut'), new Memcached('172.17.0.1'));
 $bootstrap->run();
